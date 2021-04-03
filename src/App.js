55a1716050203adfa0f4ffac58a2fcfdd3944647
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-03 23:09:59 +0900

    0403(토) | About JSX | Create 'Potato' Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+function Potato() { // Potato 컴포넌트를 만듦 (= Potato.js 생성)
+  return <h1>I like potato</h1>
+}
+
 function App() {
-  return <div>Hello!!!!!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+  )
 }
 
 export default App;
