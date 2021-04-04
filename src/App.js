c9d91c85f60b8e5109390699c3598e4b0655decf
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-04 20:45:45 +0900

    0404(일) | React | map function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div> 
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    name: 'Doncasu',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://picsum.photos/200/300',
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi"/>
+      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
     </div>
-  )
+  );
 }
 
 export default App;
