4a78aba490c6f63ab32cc6f98804a45909a3c506
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-04 23:34:03 +0900

    0404(일) | React | axios with async await

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log('hello')
-  }
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState(current => ({
-      count: current.count+1
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count-1
-    }));
-  };
-
-  componentDidMount() {
-    console.log('component rendered');
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({ movies, isLoading: false })
+  }
+  componentDidMount() { 
+    this.getMovies();
   }
 
-  componentDidUpdate() {
-    console.log('I just updated')
-  }
-
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world')
-  }
-  render() { // 함수가 아닌 클래스이기 때문에 return이 아닌 render() 함수 사용
-    console.log('component rendering')
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
   }
 }
 
