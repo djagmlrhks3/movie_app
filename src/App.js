f27deb28f8e7490ad9cd08139a46e753ff4d3923
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-04 22:31:28 +0900

    0404(일) | React | React Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('hello')
+  }
   state = {
     count: 0,
   };
@@ -17,7 +21,19 @@ class App extends React.Component {
     }));
   };
 
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated')
+  }
+
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world')
+  }
   render() { // 함수가 아닌 클래스이기 때문에 return이 아닌 render() 함수 사용
+    console.log('component rendering')
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
