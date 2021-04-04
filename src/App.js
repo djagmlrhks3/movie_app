28656103a41d1bc4a4b8f3e66a83135992ea1b6e
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-04 21:09:10 +0900

    0404(일) | React | prop-types

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div> 
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name}/>
     </div>
   );
@@ -14,26 +16,31 @@ const foodILike = [
     id: 1,
     name: 'Kimchi',
     image: 'https://picsum.photos/200/300',
+    rating: 5,
   },
   {
     id: 2,
     name: 'Samgyeopsal',
     image: 'https://picsum.photos/200/300',
+    rating: 4.9,
   },
   {
     id: 3,
     name: 'Bibimbap',
     image: 'https://picsum.photos/200/300',
+    rating: 3,
   },
   {
     id: 4,
     name: 'Doncasu',
     image: 'https://picsum.photos/200/300',
+    rating: 1.5,
   },
   {
     id: 5,
     name: 'Kimbap',
     image: 'https://picsum.photos/200/300',
+    rating: 2.5,
   },
 ];
 
@@ -41,10 +48,16 @@ function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 export default App;
