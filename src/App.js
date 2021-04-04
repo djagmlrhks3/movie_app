da049ed2ead791d1743ca05fc2533f926435c808
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-04 20:57:21 +0900

    0404(일) | React | key props

--- src/App.js
+++ src/App.js
@@ -4,29 +4,34 @@ function Food({ name, picture }) {
   return (
     <div> 
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <img src={picture} alt={name}/>
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'https://picsum.photos/200/300',
   },
   {
+    id: 2,
     name: 'Samgyeopsal',
     image: 'https://picsum.photos/200/300',
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image: 'https://picsum.photos/200/300',
   },
   {
+    id: 4,
     name: 'Doncasu',
     image: 'https://picsum.photos/200/300',
   },
   {
+    id: 5,
     name: 'Kimbap',
     image: 'https://picsum.photos/200/300',
   },
@@ -35,7 +40,9 @@ const foodILike = [
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
