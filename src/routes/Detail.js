5cab530dc5171f9e1058da04fad3b3c3f3d837ed
djagmlrhks3 <djagmlrhks3@naver.com>
2021-04-07 13:47:28 +0900

    0407(수) | React | Update Detail.js Detail.css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Detail.css'
 
 class Detail extends React.Component {
 
@@ -12,7 +13,17 @@ class Detail extends React.Component {
     render() {
         const { location } = this.props;
         if (location.state) {
-            return <span>{location.state.title}</span>
+            return <div className="movie__detail">
+                <h1>Title : {location.state.title}</h1>
+                <img src={location.state.poster} />
+                <p>{location.state.year}</p>
+                <ul className="movie__genres">
+                    {location.state.genres.map(genre => {
+                       return <li>{genre}</li> 
+                    })}
+                </ul>
+                <p>{location.state.summary}</p>
+            </div>
         } else {
             return null;
         }
